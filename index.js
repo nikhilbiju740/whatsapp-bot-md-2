64fed5d22c55d0a6ca1f725d6c80cd4949b6e5f3
HERMIT-SER <93932522+HERMIT-SIR@users.noreply.github.com>
2022-04-29 21:24:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ if (mek.key && mek.key.remoteJid === 'status@broadcast') return
 if (!bot.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
 if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
 m = smsg(bot, mek, store)
-require("./plugins/command")(bot, m, chatUpdate, store)
+require("./plugins/__")(bot, m, chatUpdate, store)
 } catch (err) {
 console.log(err)
 }
